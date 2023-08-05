1cd61e1ff6fd38dfb75e17585f45b3ec035ca4d8
Mikhail Odebe <derpiranha@gmail.com>
2023-08-06 03:45:09 +0400

    stats in ping

--- lib/actions/incoming/ping.rb
+++ lib/actions/incoming/ping.rb
@@ -4,6 +4,16 @@ module Actions
   module Incoming
     class Ping < Dispatch[TcpAction, ::Proto::Mumble::Ping]
       def handle
+        crypt = client[:crypt_state]
+
+        if crypt
+          stats = crypt.stats
+
+          message.good = stats['good']
+          message.late = stats['late']
+          message.lost = stats['lost']
+        end
+
         reply message
       end
     end
