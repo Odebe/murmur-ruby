9571c2f508cc751dc94e5576f59e7290ad746af0
Mikhail Odebe <derpiranha@gmail.com>
2023-08-05 22:11:35 +0400

    text message action refactoring

--- lib/actions/incoming/text_message.rb
+++ lib/actions/incoming/text_message.rb
@@ -3,32 +3,54 @@
 module Actions
   module Incoming
     class TextMessage < Dispatch[TcpAction, ::Proto::Mumble::TextMessage]
+      attr_reader :targets, :announce
+
       def handle
         authorize!
+        check_permission!
 
-        targets = []
+        build_announce
 
-        if message.tree_id.any?
-          reply build(:permission_denied, reason: 'Tree message not supported')
-          halt!
-        end
+        check_channel
+        check_client
 
-        if message.channel_id.any?
-          clients = db.clients.in_rooms(message.channel_id, except: [client[:session_id]])
-          clients.each { |c| targets << c }
-        end
+        send_announce
+      end
 
-        if message.session.any?
-          clients = db.clients.by_sessions(message.session)
-          clients.each { |c| targets << c }
-        end
+      private
 
+      def build_announce
+        @targets  = []
+        @announce = message
+        @announce.actor = client[:session_id]
+      end
+
+      def send_announce
         targets.uniq!
-
         return if targets.none?
 
-        message.actor = client[:session_id]
         targets.each { |t| post message, to: t }
+      end
+
+      def check_channel
+        return unless message.field?(:channel_id)
+
+        clients = db.clients.in_rooms(message.channel_id, except: [client[:session_id]])
+        targets.push(*clients)
+      end
+
+      def check_client
+        return unless message.field?(:session)
+
+        clients = db.clients.by_sessions(message.session)
+        targets.push(*clients)
+      end
+
+      def check_permission!
+        return unless message.field?(:tree_id)
+
+        reply build(:permission_denied, reason: 'Tree message not supported')
+        halt!
       end
     end
   end
