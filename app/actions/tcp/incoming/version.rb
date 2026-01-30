269f3065282556faafc2ce2f55922b2974fd3fb4
Mikhail Dmitriev <derpiranha@gmail.com>
2026-01-30 18:43:21 +0400

    server version

--- app/actions/tcp/incoming/version.rb
+++ app/actions/tcp/incoming/version.rb
@@ -3,13 +3,18 @@
 module Actions
   module Tcp
     module Incoming
-      PROTOBUF_INTRODUCTION_VERSION = "1.5.0"
-
       class Version < Dispatch[TcpAction, ::Proto::Mumble::Version]
+        MUMBLE_PROTOCOL_VERSION = Gem::Version.new('1.5.0')
+
+        VERSION_V1 = Client::Version.to_v1(MUMBLE_PROTOCOL_VERSION)
+        VERSION_V2 = Client::Version.to_v2(MUMBLE_PROTOCOL_VERSION)
+
         def handle
           app.db.clients.set_version(client[:session_id], message)
 
-          # TODO: send server version
+          message.version_v2 = VERSION_V2
+          message.version_v1 = VERSION_V1
+
           reply message
         end
       end
